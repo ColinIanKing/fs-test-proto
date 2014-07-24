ca44472a2a32369e43e20fd1a92502d3a6e56754
Colin Ian King <colin.king@canonical.com>
2014-07-24 23:59:33 +0100

    Remove extraneous - in perf command
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- fio-tests/fio.sh
+++ fio-tests/fio.sh
@@ -109,7 +109,7 @@ log_job_info()
 stats()
 {
 	if [ ${SCENARIO} == "perf" ]; then
-		perf record -o ${RESULTS_PATH}/perf.data -F ${PERF_FREQ} -g - ${ROOT_PATH}/perf-wrapper.sh -e RUNTIME=${RUNTIME} -e LOG_AVG_MSEC=${LOG_AVG_MSEC} -e BLOCKSIZE=${BLOCKSIZE} -e SIZE=${SIZE} -e DIRECTORY=${DIRECTORY} $FIO $* ${ROOT_PATH}/jobs/${JOB} --output-format=json --output=${RESULTS_PATH}/fio-stats.json
+		perf record -o ${RESULTS_PATH}/perf.data -F ${PERF_FREQ} -g ${ROOT_PATH}/perf-wrapper.sh -e RUNTIME=${RUNTIME} -e LOG_AVG_MSEC=${LOG_AVG_MSEC} -e BLOCKSIZE=${BLOCKSIZE} -e SIZE=${SIZE} -e DIRECTORY=${DIRECTORY} $FIO $* ${ROOT_PATH}/jobs/${JOB} --output-format=json --output=${RESULTS_PATH}/fio-stats.json
 		perf report -i ${RESULTS_PATH}/perf.data > ${RESULTS_PATH}/perf.report
 		perf script -i ${RESULTS_PATH}/perf.data > ${RESULTS_PATH}/perf.script
 		gzip --best ${RESULTS_PATH}/perf.data ${RESULTS_PATH}/perf.report ${RESULTS_PATH}/perf.script
